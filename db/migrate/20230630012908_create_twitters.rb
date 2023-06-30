7953674cecefe064ec265d3975bd753c3d9edede
Nindecita <parancibiac@gmail.com>
2023-06-29 21:53:23 -0400

    first commit

---
+++ db/migrate/20230630012908_create_twitters.rb
@@ -0,0 +1,10 @@
+class CreateTwitters < ActiveRecord::Migration[7.0]
+  def change
+    create_table :twitters do |t|
+      t.string :Description
+      t.string :userName
+
+      t.timestamps
+    end
+  end
+end
